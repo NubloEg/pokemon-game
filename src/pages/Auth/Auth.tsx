e90e2f29811232b5d01753aebcd4bcb0153fb9bc
Egor Dovgalev <egor.dovgalev.anthill@gmail.com>
2023-10-15 17:07:03 +0300

    changes

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -6,6 +6,7 @@ import Loading from "../../components/Loading/Loading";
 
 export default function Auth() {
   const navigate = useNavigate();
+  const [isLogin, setIsLogin] = useState(true)
   const [profile, setProfile] = useState<{ login: string; password: string }>({
     login: "",
     password: "",
@@ -16,25 +17,59 @@ export default function Auth() {
   });
 
   const signUp = () => {
-    fetch(`http://localhost:5000/auth/${profile.login}`)
+    fetch(`http://localhost:5000/auth/sign`, {
+      method: "post",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        login: profile.login,
+        password: profile.password
+      })
+    })
       .then((response) => response.json())
       .then((data) => {
-        sessionStorage.setItem('profile', JSON.stringify(data[0]))
-        navigate('/firstpokemon')
+        if(data.login){
+          sessionStorage.setItem('profile', JSON.stringify(data))
+          navigate('/firstpokemon')
+        }
       })
       .catch((e) => {
         alert("Пользователь не найден");
       });
   };
 
+  const register = () => {
+    fetch(`http://localhost:5000/auth`, {
+      method: "post",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        login: profile.login,
+        password: profile.password
+      })
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        sessionStorage.setItem('profile', JSON.stringify(data))
+        navigate('/firstpokemon')
+      })
+      .catch((e) => {
+        alert("Ошибка зарегестрировать пользователя не удалось");
+      });
+  };
+
   return (
     <>
       <div className={s.authMain}></div>
-      <div>Register</div>
       <div className={s.form}>
         <h1 className={s.title}>Welcome</h1>
         <div className={s.relative}>
-          <div className={s.auth}>
+          <div onClick={() => setIsLogin(!isLogin)} className={`${s.switch} ${!isLogin && s.change}`}>{!isLogin ? "Login" : "Register"}</div>
+          <div className={`${s.auth} ${!isLogin && s.close}`}>
             <div className={s.name}>
               <div className={s.name__title}>Name</div>
               <input
@@ -56,7 +91,7 @@ export default function Auth() {
               />
             </div>
           </div>
-          <div className={s.register}>
+          <div className={`${s.register} ${!isLogin && s.open}`}>
             <div className={s.name}>
               <div className={s.name__title}>Name</div>
               <input
@@ -80,7 +115,7 @@ export default function Auth() {
           </div>
         </div>
 
-        <Button onClick={() => signUp()} >Login</Button>
+        <Button onClick={isLogin ? () => signUp() : () => register()} >{isLogin ? "Login" : "Register"}</Button>
       </div>
       <Loading loading={loading} />
     </>
